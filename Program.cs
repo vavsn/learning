79fe25c20005134e79de7cfb8f17207bc1af34f5
Andrey <vavsn@mail.ru>
2021-05-17 18:51:51 +0700

    Lesson-1 Homework

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InputUserName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // переменная для хранения имени пользователя
+            string UserName = string.Empty;
+            Console.Write("Введите свое имя: ");
+            UserName = Console.ReadLine();
+            if (UserName == string.Empty)
+                UserName = Environment.UserName;
+            Console.Write($"Привет, {UserName}, сегодня {DateTime.Now.ToString("dd MMMM yyyy")} г.");
+        }
+}
+}
