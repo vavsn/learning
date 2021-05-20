0d1b56aa5cd370951e1716e3fe463e710273309b
vavsn <41681585+vavsn@users.noreply.github.com>
2021-05-20 21:54:32 +0700

    Add files via upload
    Lesson-7
    Homework

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 
-namespace InputUserName
+namespace Lesson7
 {
     class Program
     {
+        /// <summary>
+        /// выполняем задание урока 7
+        /// </summary>
         static void Main(string[] args)
         {
-            // переменная для хранения имени пользователя
-            string UserName = string.Empty;
-            Console.Write("Введите свое имя: ");
-            UserName = Console.ReadLine();
-            if (UserName == string.Empty)
-                UserName = Environment.UserName;
-            Console.Write($"Привет, {UserName}, сегодня {DateTime.Now.ToString("dd MMMM yyyy")} г.");
+            // просим пользователя ввести строку
+            Console.WriteLine("Введите строку:");
+
+            // преобразуем строку в массив символов
+            char[] symbols = Console.ReadLine().ToCharArray();
+
+            // переменная для вывода информации на экран
+            Console.WriteLine("Выполняем задание:");
+            for (int i = symbols.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}", symbols[i]);
+            }
+            Console.WriteLine();
         }
-}
+    }
 }
